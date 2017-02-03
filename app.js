57f8f0c1225e68fc105d2b4d4fb5753e6b6ed17b
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-02-03 01:48:40 -0200

    feature: Move user update storage to promise

--- app.js
+++ app.js
@@ -4,56 +4,115 @@ const messages = require('./src/messages');
 const menu = require('./src/menu');
 const helpers = require('./src/helpers');
 
-let store = [];
+let storage = [];
 
+const setUserStorage = updateEvent => {
+  return new Promise((resolve, reject) => {
+    try{
+      if("undefined" === typeof storage[updateEvent.update.sender.id]){
+        let userInfo = mybot.getUserInfo(updateEvent.update.sender.id);
+        userInfo.then(data => {
+          storage[updateEvent.update.sender.id] = {
+            id: updateEvent.update.sender.id,
+            first_name: data.first_name,
+            last_name: data.last_name,
+            profile_pic: data.profile_pic,
+            locale: data.locale,
+            gender: data.gender,
+            timezone: data.timezone,
+            step: 0,
+            locate: null,
+            time: Date.now()
+          }
+          resolve()
+        })
+      } else {
+        resolve()
+      }
+    } catch(e){
+      reject(e)
+    }
+  })
+}
 
 const myMessageListener = function(updateEvent){
-    console.log('received message:', updateEvent.update.message.text);
-    // reply with the same received message
-    //let userInfo = mybot.getUserInfo(updateEvent.update.sender.id);
-    //userInfo.then(data => {
-    //})
+  setUserStorage(updateEvent)
+  .then(() => {
+    let { id: userId, first_name: name, step, locate } = storage[updateEvent.update.sender.id],
+        messageText = updateEvent.update.message.text;
+
+    switch(true){
+      case step == 'a' && locate === 1:
+        messageText = `busca url ${updateEvent.update.message.text}`
+      break;
+    }
 
     updateEvent.bot.sendMessage({
-        userId: updateEvent.update.sender.id,
-        text:'mensagem texto'
+        userId,
+        text: messageText
     })
-
+  })
 };
 
 const onPostback = function(updateEvent){
-    let postback = updateEvent.update.postback;
-    let payload = updateEvent.update.postback.payload;
+    setUserStorage(updateEvent)
+    .then(()=>{
+      let payload = updateEvent.update.postback.payload;
+      console.log('onPostback', updateEvent.update.sender.id, payload)
+      let { id: userId, first_name: name } = storage[updateEvent.update.sender.id];
 
-    console.log('onPostback', postback, payload)
-
-    if(helpers.isJSON(payload) && helpers.getJSON(payload).type === 'legacy-welcome'){
-
-      updateEvent.bot.sendMessage({
-        userId: updateEvent.update.sender.id,
-        attachment:{
-        type:"template",
-        payload:{
-          template_type:"button",
-          text: messages('stage0'),
-          buttons:[
-            {
-              type:"postback",
-              title:"Cadastrar um podcast",
-              payload:'a'
-            },
-            {
-              type:"postback",
-              title:"Ouvir um podcast",
-              payload:"b"
+      switch(true){
+        case helpers.isJSON(payload) &&
+        helpers.getJSON(payload).type === 'legacy-welcome':
+            updateEvent.bot.sendMessage({
+              userId: updateEvent.update.sender.id,
+              attachment:{
+              type:"template",
+              payload:{
+                template_type:"button",
+                text: messages('stage0'),
+                buttons:[
+                  {
+                    type:"postback",
+                    title:"Cadastrar um podcast",
+                    payload:'a'
+                  },
+                  {
+                    type:"postback",
+                    title:"Ouvir um podcast",
+                    payload:"b"
+                  }
+                ]
+              }
             }
-          ]
-        }
+          });
+        break;
+        case payload == 'a':
+          updateEvent.bot.sendMessage({
+              userId,
+              text:`Ok, ${name}! Envie a url que você deseja adicionar`
+          });
+
+          storage[updateEvent.update.sender.id].step = 'a';
+          storage[updateEvent.update.sender.id].locate = 1;
+          console.log('escolheu cadastrar')
+        break;
+        case payload == 'b':
+          storage[updateEvent.update.sender.id].step = 'b';
+          storage[updateEvent.update.sender.id].locate = 1;
+          console.log('escolheu ouvir')
+        break;
       }
-    });
+    })
 
 
-  }
+
+
+
+
+
+
+
 
 
 }
@@ -74,20 +133,6 @@ mybot.start().then(function(){
     // Get Started button reply
     mybot.setWelcomeMessage({
         text: messages('greeting')
-      //   quick_replies:[
-      //   {
-      //     content_type:"text",
-      //     title:"Red",
-      //     payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED",
-      //     image_url:"http://petersfantastichats.com/img/red.png"
-      //   },
-      //   {
-      //     content_type:"text",
-      //     title:"Green",
-      //     payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN",
-      //     image_url:"http://petersfantastichats.com/img/green.png"
-      //   }
-      // ]
     }).then(welcomeMsgSetResult =>
         console.log('welcomeMsgSetResult', welcomeMsgSetResult)
     ).catch(e => {
